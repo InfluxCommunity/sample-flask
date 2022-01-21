c2921c47b2f2b1de65fd2995ab8494ad3a305de2
pierwill <pierwill@users.noreply.github.com>
2022-01-21 11:35:39 -0600

    Remove extra whitespace

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 organization_name = os.environ["INFLUXDB_ORGANIZATION"]
 
 # The host URL for where your instance of InfluxDB runs. This is also the URL where you reach the UI for your account.
-host = os.environ["INFLUXDB_HOST"] 
+host = os.environ["INFLUXDB_HOST"]
 
 # An appropriately scoped token or set of tokens. For ease of use in this example, we will use an all access token.
 # Note that you should not store the token in source code in a real application, but rather use a proper secrets store.
@@ -38,7 +38,7 @@ host = os.environ["INFLUXDB_HOST"]
 # https://docs.influxdata.com/influxdb/v2.1/security/tokens/
 token = os.environ["INFLUXDB_TOKEN"]
 
-# A bucket name is required for the write_api. A bucket is where you store data, and you can 
+# A bucket name is required for the write_api. A bucket is where you store data, and you can
 # group related data into a bucket. You can also scope permissions to the bucket level as well.
 bucket_name = "raw_data_bucket"
 
@@ -81,7 +81,7 @@ def ingest():
         .tag("user_id", user_id) \
         .time(datetime.utcnow(), WritePrecision.NS)
 
-    # If you don't want to post the data, you can uncomment the following, and delete the above code    
+    # If you don't want to post the data, you can uncomment the following, and delete the above code
     # point = Point("measurement1") \
     #     .field("field1", 1.0) \
     #     .tag("user_id", "user1")\
@@ -126,7 +126,7 @@ def query():
     tables = query_api.query(query, org=organization_name, params=params)
 
     # the data will be returned as Python objects so you can iterate the data and do what you want
-    for table in tables: 
+    for table in tables:
         for record in table.records:
             print(record)
 
@@ -137,7 +137,7 @@ def query():
 @app.route("/visualize", methods=["GET"])
 def visualize():
     # This function returns a visualization (graph) instead of just data
-    
+
     # Your real code should authorize the user, and ensure that the user_id matches the authorization.
     # Send the user name as an argument from the web browser:
     # 127.0.0.1:5001/visualize?user_name=user1
@@ -152,12 +152,12 @@ def visualize():
 
     # This example users plotly and pandas to create the visualization
     # You can learn more about using InfluxDB with Pandas by following this link:
-    # 
+    #
     # InfluxDB supports any visualization library you choose, you can learn more about visualizing data following this link:
-    # 
+    #
     data_frame = query_api.query_data_frame(query, organization_name, params=params)
     graph = px.line(data_frame, x="_time", y="_value", title="my graph")
-    
+
     return graph.to_html(), 200
 
 @app.route("/tasks", methods=["POST"])
@@ -166,25 +166,25 @@ def tasks():
     # {"user_id":"user1"}
     # or uncomment the following line if you choose not to post it:
     # user_id = "user1"
-    
+
     # ensure there is a bucket to copy the data into
     find_or_create_bucket("processed_data_bucket")
 
-    # The follow flux will find any values in the specified time range that have a 
+    # The follow flux will find any values in the specified time range that have a
     # value of 0.0 and will copy those points into a special bucket. This demonstrates 2 concepts:
     # 1. "downsampling", or the ability to easily precompute data so that you can supply low latency
     # queries for your UI. For more on downsampling, see:
     # https://awesome.influxdata.com/docs/part-2/querying-and-data-transformations/#materialized-views-or-downsampling-tasks
     # 2. "alerting", or the ability to send a notification based on certain values and conditions.
     # For example, rather than writing the data to a new bucket, you can use http.post() to call back your application
-    # or a different service. 
+    # or a different service.
     # Tho see the full power of the alerting system, follow this link:
     # https://awesome.influxdata.com/docs/part-3/checks-and-notifications/
     query = """
 option task = {{name: "{}_task", every: 1m}}
 
  from(bucket: "{}")
- |> range(start: -1m)  
+ |> range(start: -1m)
  |> filter(fn: (r) => r.user_id == "{}")
  |> filter(fn: (r) => r._value == 0.0)
  |> to(bucket: "processed_data_bucket")
@@ -228,8 +228,8 @@ def monitor():
 
     # Your own code should verify that the person viewing this has proper authorization
 
-    # The following flux query will retrieve the 3 kinds of usage data available 
-    # and combine the data into a single table for ease of formatting. 
+    # The following flux query will retrieve the 3 kinds of usage data available
+    # and combine the data into a single table for ease of formatting.
     # For more information about the usage.from() function, see the following:
     # https://docs.influxdata.com/flux/v0.x/stdlib/experimental/usage/from/
 
@@ -250,7 +250,7 @@ usage.from(start: -1h, stop: now())
             html += f"<TR><TD>{mes}</TD><TD>{val}</TD></TR>"
     html += "</TABLE>"
 
-    # This part of the function looks at task health. 
+    # This part of the function looks at task health.
     # Tasks allow you to run code periodically within InfluxDB. For an overview of tasks, see the following:
     # https://docs.influxdata.com/influxdb/cloud/process-data/get-started/
 
@@ -264,7 +264,7 @@ usage.from(start: -1h, stop: now())
     tasks = tasks_api.find_tasks()
 
     html += "<H1>tasks</H1><TABLE><TR><TH>name</TH><TH>status</TH><TH>last run</TH><TH>last run status</TH></TR>"
-    
+
     # Each task has a run log, accessed through the get_runs() function
     # This code checks if each task is enabled, and if so, checks the status of its last run
     # For active tasks, format the status report
@@ -298,12 +298,12 @@ def find_or_create_bucket(bucket_to_find_or_create):
     # policu will automatically be deleted and cleaned up by InfluxDB.
     # You can read more about buckets and retention policy by following this link:
     # https://docs.influxdata.com/influxdb/cloud/organizations/buckets/
-    
+
     try:
         # use the buckets api to find a bucket by its name
         bucket = client.buckets_api().find_bucket_by_name(bucket_to_find_or_create)
         print(f"bucket ({bucket_to_find_or_create}) found with retention policy: {bucket.rp}")
-        
+
     except ApiException as e:
         # The most likely problem is that the bucket does not exist, in which case add it
         # Check the status code for other possible errors that you wish to handle
